1faefaebe364aa9bcbfa7100dab52294d6a1fc9a
Mantasha Ansari <120690932+mantu0tech@users.noreply.github.com>
2026-07-29 08:30:08 +0530

    Delete dcompose_data_django directory

--- dcompose_data_django/message.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS to_do_list_db;
-USE to_do_list_db;
-CREATE TABLE messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    message TEXT
-);
-
--- dcompose_data_django/to_do_list_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('my_to_do_list_app.urls')),
-]
